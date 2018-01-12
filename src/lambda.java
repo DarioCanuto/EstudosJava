7cbe6111212985e03ae8f255e11ff56890a5fec6
DarioCanuto <dario_canuto@hotmail.com>
2018-01-12 00:32:11 -0200

    lambdas

---
+++ src/lambda.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class lambda {
+	public static void main(String[] args) {
+		Usuario user1 = new Usuario("Dario",150);
+		Usuario user2 = new Usuario("Thamires",200);
+		Usuario user3 = new Usuario("Daniel",250);
+		
+		List<Usuario> usuarios = Arrays.asList(user1,user2,user3);
+		
+//		usuarios.forEach(new Consumer<Usuario>(){
+//			public void accept(Usuario u){			
+//				System.out.println(u.getNome());
+//			}
+//		
+//		});
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+	}
+}
+
